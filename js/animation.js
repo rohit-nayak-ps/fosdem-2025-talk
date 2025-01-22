bd9025a76a1adfce88fba0f5730f601285f30f4e
Rohit Nayak <rohit@planetscale.com>
2025-01-22 23:33:02 +0100

    Correct Step 0 connections. Add Step 2 arrows, partly correct
    Signed-off-by: Rohit Nayak <rohit@planetscale.com>

--- js/animation.js
+++ js/animation.js
@@ -86,36 +86,55 @@ class StepManager {
         const vtctld = document.querySelector('.vtctld-block');
         const shard1Tablet = document.querySelector('.shard-1 .vttablet-block');
         const shard2Tablet = document.querySelector('.shard-2 .vttablet-block');
+        const rds = document.querySelector('.rds-cluster');
+        const shard1 = document.querySelector('.shard-1');
+        const shard2 = document.querySelector('.shard-2');
 
         const lineVtgateShard1 = document.getElementById('line-vtgate-shard1');
         const lineVtgateShard2 = document.getElementById('line-vtgate-shard2');
-        const lineVtorcEtcd = document.getElementById('line-vtorc-etcd');
-        const lineVtgateEtcd = document.getElementById('line-vtgate-etcd');
-        const lineVtctldEtcd = document.getElementById('line-vtctld-etcd');
-        const lineVtctldShard1 = document.getElementById('line-vtctld-shard1');
-        const lineVtctldShard2 = document.getElementById('line-vtctld-shard2');
 
-        this.connectElements(vtgate, shard1Tablet, lineVtgateShard1, container);
-        this.connectElements(vtgate, shard2Tablet, lineVtgateShard2, container);
-        this.connectElements(vtorc, etcd, lineVtorcEtcd, container);
-        this.connectElements(vtgate, etcd, lineVtgateEtcd, container);
-        this.connectElements(vtctld, etcd, lineVtctldEtcd, container);
-        this.connectElements(vtctld, shard1Tablet, lineVtctldShard1, container);
-        this.connectElements(vtctld, shard2Tablet, lineVtctldShard2, container);
+
+        this.connectElements(vtgate, shard1Tablet, lineVtgateShard1, container, 20, -5);
+        this.connectElements(vtgate, shard2Tablet, lineVtgateShard2, container, 20, 5);
 
         gsap.fromTo(
             lineVtgateShard1,
             {strokeDashoffset: 100},
-            {strokeDashoffset: 0, duration: 1, ease: 'power1.inOut'}
+            {strokeDashoffset: 1, duration: 5, ease: 'power1.inOut', yoyo: true, repeat: -1}
         );
         gsap.fromTo(
             lineVtgateShard2,
             {strokeDashoffset: 100},
-            {strokeDashoffset: 0, duration: 1, ease: 'power1.inOut'}
+            {strokeDashoffset: 1, duration: 5, ease: 'power1.inOut', yoyo: true, repeat: -1}
         );
+
+        const lineRdsShard1Vertical = document.getElementById('line-rds-shard1-vertical');
+        const lineRdsShard1Horizontal = document.getElementById('line-rds-shard1-horizontal');
+        const lineRdsShard2Vertical = document.getElementById('line-rds-shard2-vertical');
+        const lineRdsShard2Horizontal = document.getElementById('line-rds-shard2-horizontal');
+
+        this.connectRightAngle(rds, shard1, lineRdsShard1Vertical, lineRdsShard1Horizontal, container);
+        this.connectRightAngle(rds, shard2, lineRdsShard2Vertical, lineRdsShard2Horizontal, container);
     }
 
-    connectElements(fromEl, toEl, lineEl, containerEl) {
+
+    connectElements(fromEl, toEl, lineEl, containerEl, offsetX, offsetY) {
+        const fromRect = fromEl.getBoundingClientRect();
+        const toRect = toEl.getBoundingClientRect();
+        const cRect = containerEl.getBoundingClientRect();
+
+        const x1 = fromRect.right - cRect.left;
+        const y1 = (fromRect.top + fromRect.bottom) / 2 - cRect.top;
+        const x2 = toRect.left - cRect.left;
+        const y2 = (toRect.top + toRect.bottom) / 2 - cRect.top;
+
+        lineEl.setAttribute('x1', x1 );
+        lineEl.setAttribute('y1', y1 + offsetY);
+        lineEl.setAttribute('x2', x2 - offsetX);
+        lineEl.setAttribute('y2', y2 );
+    }
+
+    connectRightAngle(fromEl, toEl, verticalLineEl, horizontalLineEl, containerEl) {
         const fromRect = fromEl.getBoundingClientRect();
         const toRect = toEl.getBoundingClientRect();
         const cRect = containerEl.getBoundingClientRect();
@@ -125,12 +144,19 @@ class StepManager {
         const x2 = (toRect.left + toRect.right) / 2 - cRect.left;
         const y2 = (toRect.top + toRect.bottom) / 2 - cRect.top;
 
-        lineEl.setAttribute('x1', x1);
-        lineEl.setAttribute('y1', y1);
-        lineEl.setAttribute('x2', x2);
-        lineEl.setAttribute('y2', y2);
+        verticalLineEl.setAttribute('x1', x1);
+        verticalLineEl.setAttribute('y1', y1);
+        verticalLineEl.setAttribute('x2', x1);
+        verticalLineEl.setAttribute('y2', y2);
+
+        horizontalLineEl.setAttribute('x1', x1);
+        horizontalLineEl.setAttribute('y1', y2);
+        horizontalLineEl.setAttribute('x2', x2);
+        horizontalLineEl.setAttribute('y2', y2);
     }
 }
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
     new StepManager();
